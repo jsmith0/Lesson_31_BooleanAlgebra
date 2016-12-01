05fe260ad3cc49a18337f983eff54382a2ab03f0
js456 <justins4@comcast.net>
2016-12-01 10:31:15 -0500

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,24 @@
+package com.company;
+
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        //this is actually my TWIP
+
+        Random rnd  = new Random();
+        int randnum = 0 + rnd.nextInt(101);
+        boolean finished = false;
+        int[] newint = new int[100];
+        while(!finished) {
+            for (int k = 0; k < 100; k++)
+            //while (newint is not full)
+            {
+                System.out.println(randnum);
+            }
+        }
+    }
+}
